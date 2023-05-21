c7480680cf7f0be46d3d37dc5e1ed70fbe2b6e5c
Dmytro Morozov <morozovdima6@gmail.com>
2023-05-21 18:35:43 +0300

    The entity of user and table in MySQL was created.

---
+++ src/main/java/com/ua/vinnytsia/airbaltic_task/AirBalticTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ua.vinnytsia.airbaltic_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBalticTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBalticTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/vinnytsia/airbaltic_task/AirBalticTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.vinnytsia.airbaltic_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBalticTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
