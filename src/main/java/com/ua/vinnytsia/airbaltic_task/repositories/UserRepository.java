d2dfb216080b05b0e7b05d62568630538beec0ae
Dmytro Morozov <morozovdima6@gmail.com>
2023-05-22 22:32:21 +0300

    It was added methods updateUser, getUser, allUser to the service layer and related controllers. The checking  for non-existent users was added to all methods of service layer.

--- src/main/java/com/ua/vinnytsia/airbaltic_task/repositories/UserRepository.java
+++ src/main/java/com/ua/vinnytsia/airbaltic_task/repositories/UserRepository.java
@@ -4,12 +4,10 @@ import com.ua.vinnytsia.airbaltic_task.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> getUserById(Long id);
-//    List<User> findAllUsers();
 }
