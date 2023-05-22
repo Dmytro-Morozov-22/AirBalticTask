fa1712ebfda71133aa6327dfe3bb43f9bffef3f1
Dmytro Morozov <morozovdima6@gmail.com>
2023-05-22 22:30:35 +0300

    It was added methods updateUser, getUser, allUser to the service layer and related controllers. The checking  for non-existent users was added to all methods of service layer.

--- src/main/java/com/ua/vinnytsia/airbaltic_task/controllers/UserController.java
+++ src/main/java/com/ua/vinnytsia/airbaltic_task/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.ua.vinnytsia.airbaltic_task.controllers;
 
 import com.ua.vinnytsia.airbaltic_task.dto.request.UserDto;
+import com.ua.vinnytsia.airbaltic_task.models.User;
 import com.ua.vinnytsia.airbaltic_task.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -19,6 +22,21 @@ public class UserController {
     public ResponseEntity<?> saveUser(@ModelAttribute UserDto userDto) {
         return ResponseEntity.ok(userService.saveUser(userDto));
     }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getUser(@PathVariable("id") Long userId){
+        return ResponseEntity.ok(userService.getUser(userId));
+    }
+
+    @GetMapping("/all")
+    public List<User> getUsers(){
+        return userService.allUsers();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable("id") Long userId, @ModelAttribute UserDto userDto) {
+        return ResponseEntity.ok(userService.updateUser(userId, userDto));
+    }
+
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String>deleteUser(@PathVariable("id") Long userId) {
         return ResponseEntity.ok(userService.deleteUser(userId));
--- src/main/java/com/ua/vinnytsia/airbaltic_task/services/UserService.java
+++ src/main/java/com/ua/vinnytsia/airbaltic_task/services/UserService.java
@@ -1,6 +1,5 @@
 package com.ua.vinnytsia.airbaltic_task.services;
 
-
 import com.ua.vinnytsia.airbaltic_task.dto.request.UserDto;
 import com.ua.vinnytsia.airbaltic_task.dto.response.UserInfoDto;
 import com.ua.vinnytsia.airbaltic_task.models.User;
@@ -8,6 +7,8 @@ import com.ua.vinnytsia.airbaltic_task.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -23,17 +24,35 @@ public class UserService {
                 userDto.getLastName(),userDto.getEmail()));
             return new UserInfoDto(savedUser.getId(), savedUser.getFirstName(), savedUser.getLastName(), savedUser.getEmail());
         }
-        throw new ResponseStatusException(420, "The" + userDto.getEmail() + "is already in the system", null);
+        throw new ResponseStatusException(420, "The " + userDto.getEmail() + " is already in the system", null);
     }
 
     private boolean isEmailUnique(String email) {
         return userRepository.findByEmail(email).isEmpty();
     }
 
+    public User getUser(Long userId) {
+        return userRepository.getUserById(userId)
+            .orElseThrow(() -> new ResponseStatusException(404, "User with id " + userId + " not found", null));
+    }
 
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
 
-    public String deleteUser(Long id){
-        userRepository.deleteById(id);
+    public User updateUser(Long userId, UserDto userDto) {
+        User user = userRepository.getUserById(userId)
+            .orElseThrow(() -> new ResponseStatusException(404, "User with id " + userId + " not found", null));
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        return userRepository.save(user);
+    }
+
+    public String deleteUser(Long userId){
+        userRepository.findById(userId)
+            .orElseThrow(() -> new ResponseStatusException(404, "User with id " + userId + " not found", null));
+        userRepository.deleteById(userId);
         return "User successfully deleted!";
     }
 
